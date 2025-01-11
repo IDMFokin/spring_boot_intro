86489584c26ca03de161df4c75618920022bcff6
Fokin <Fokin1@ukr.net>
2025-01-11 15:56:49 -0700

    mkd .github/workflows and file ci.yml

--- src/main/java/mate/academy/SpringBootIntroApplication.java
+++ src/main/java/mate/academy/SpringBootIntroApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootIntroApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SpringBootIntroApplication.class, args);
     }
-
 }
