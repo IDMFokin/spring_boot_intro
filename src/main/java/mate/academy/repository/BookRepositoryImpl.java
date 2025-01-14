8fceb6560dd67ca0e91a91479975fe9a519069d6
Fokin <Fokin1@ukr.net>
2025-01-13 17:47:28 -0700

    -create interface BookRepository and BookServise. -create class BookRepositoryImpl and BookServiceImpl

---
+++ src/main/java/mate/academy/repository/BookRepositoryImpl.java
@@ -0,0 +1,54 @@
+
+package mate.academy.repository;
+
+import java.util.List;
+import java.util.Objects;
+import mate.academy.model.Book;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookRepositoryImpl implements BookRepository {
+
+    private SessionFactory sessionFactory;
+
+    @Autowired
+    public BookRepositoryImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public Book save(Book book) {
+        Session session = null;
+        Transaction transaction = null;
+        Objects.requireNonNull(book, "Book must not be null");
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(book);
+            transaction.commit();
+            return book;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't insert book into DB: " + book, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<Book> findAll() {
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("SELECT b FROM Book b", Book.class).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get all books from DB", e);
+        }
+    }
+}
